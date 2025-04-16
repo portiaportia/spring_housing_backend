56093970b9e5a46fb27d3757e47c6dd8ee545955
portiaportia <portiaplante@gmail.com>
2025-04-16 10:12:14 -0400

    db working

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const houseSchema = new mongoose.Schema({
   size:Number,
   bedrooms:Number,
   bathrooms:Number,
-  main_images:String
+  main_image:String
 });
 
 const House = mongoose.model("House", houseSchema);
@@ -41,81 +41,6 @@ const House = mongoose.model("House", houseSchema);
 app.get("/",(req, res)=>{
     res.sendFile(__dirname+"/index.html");
 });
-
-/*
-let houses = [
-    {
-    "_id":1,
-    "name": "Farmhouse",
-    "size": 2000,
-    "bedrooms": 3,
-    "bathrooms": 2.5,
-    "features": [
-    "wrap around porch",
-    "attached garage"
-    ],
-    "main_image": "farm.webp",
-    "floor_plans": [
-    {
-    "name": "Main Level",
-    "image": "farm-floor1.webp"
-    },
-    {
-    "name": "Basement",
-    "image": "farm-floor2.webp"
-    }
-    ]
-    },
-    {
-    "_id":2,
-    "name": "Mountain House",
-    "size": 1700,
-    "bedrooms": 3,
-    "bathrooms": 2,
-    "features": [
-    "grand porch",
-    "covered deck"
-    ],
-    "main_image": "mountain-house.webp",
-    "floor_plans": [
-    {
-    "name": "Main Level",
-    "image": "mountain-house1.webp"
-    },
-    {
-    "name": "Optional Lower Level",
-    "image": "mountain-house2.webp"
-    },
-    {
-    "name": "Main Level Slab Option",
-    "image": "mountain-house3.jpg"
-    }
-    ]
-    },
-    {
-    "_id":3,
-    "name": "Lake House",
-    "size": 3000,
-    "bedrooms": 4,
-    "bathrooms": 3,
-    "features": [
-    "covered deck",
-    "outdoor kitchen",
-    "pool house"
-    ],
-    "main_image": "farm.webp",
-    "floor_plans": [
-    {
-    "name": "Main Level",
-    "image": "lake-house1.webp"
-    },
-    {
-    "name": "Lower Level",
-    "image": "lake-house2.webp"
-    }
-    ]
-    }
-]; */
 
 app.get("/api/houses", async(req, res) => {
   const houses = await House.find();
@@ -139,6 +64,9 @@ app.post("/api/houses", upload.single("img"), async(req, res) => {
   });
 
   if (req.file) {
+    console.log("Showing the file");
+    console.log(req.file.filename);
+
     house.main_image = req.file.filename;
   }
 
